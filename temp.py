5410e7ffdf9ec7b569262e1d005a598b00a43f6b
arnav-exe <arnav.jagetia03@gmail.com>
2026-01-30 22:46:07 +0000

    cross os node testing

--- temp.py
+++ temp.py
@@ -1,13 +1,35 @@
 import subprocess
 import sys
+import os
 
 
-def main():
+PRODUCTS = [
+    {  # airpods pro 3
+        "bestbuy_sku": 6376563,
+        "amazon_asin": "B0FQFB8FMG",
+        "desired_price": 200,
+        "ntfy_topic": os.getenv("NTFY_TOPIC_URL")
+    },
+    {  # lenovo legion go 2
+        "sku": 6643145,
+        "desired_price": 1500,
+        "ntfy_topic": os.getenv("NTFY_TOPIC_URL")
+    },
+    {  # LG 27 inch 1440p 180hz monitor (TESTING)
+        "sku": 6575404,
+        "desired_price": 400,
+        "ntfy_topic": os.getenv("NTFY_TOPIC_URL")
+    }
+]
+
+
+def main(asin):
     if sys.platform == "win32":
-        subprocess.run(["npx.cmd", "amazon-buddy"])
+        subprocess.run(f"node_modules\\.bin\\amazon-buddy.cmd asin {asin}")
     else:
-        subprocess.run(["npx", "amazon-buddy"])
+        subprocess.run(f"node_modules\\.bin\\amazon-buddy asin {asin}")
 
 
 if __name__ == "__main__":
-    main()
+    asin = PRODUCTS[0]["amazon_asin"]
+    main(asin)
