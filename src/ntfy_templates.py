e837d69bb26d0e4b01678ebc42877b4bfd469286
arnav-exe <arnav.jagetia03@gmail.com>
2026-02-15 00:00:30 -0500

    updated ntfy templates to handle 3 different conditions for firing noti

--- src/ntfy_templates.py
+++ src/ntfy_templates.py
@@ -1,110 +1,56 @@
-from datetime import datetime
-from dateutil.parser import parse
+def on_sale(product, desired_price: float):
+    target_discount = (1 - (p.regular_price / desired_price)) * 100
+
+    return f"""### {product.product_name} is in stock AND on sale!
+
+**💰 Sale Price:** ${product.sale_price:.2f}  
+**🏷️ Original Price:** ${product.regular_price:.2f}  
+**🎯 Your Target Price:** ${desired_price:.2f}  
+
+**💵 Sale savings:** ${product.dollar_savings:.2f} ({product.percent_savings:.1f}%)  
+**💵 Below your target by:** ${(desired_price - product.sale_price):.2f} ({target_discount:.1f}%)
+
+🔗 [{product.product_url}]({product.product_url})"""
 
 
-def get_days_hours(seconds: float):
-    days, seconds = divmod(seconds, 86400)
-    hours = round(seconds / 3600)
+def below_max_price(product, desired_price: float):
+    target_discount = (1 - (p.regular_price / desired_price)) * 100
 
-    return days, hours
+    return f"""### {product.product_name} is in stock AND within your price target!
 
+**💰 Current Price:** ${product.regular_price:.2f}  
+**🎯 Your Target Price:** ${desired_price:.2f}  
 
-def calc_time_diff(time_str):
-    time_obj = parse(time_str)
-    current_time = parse(datetime.now().isoformat())
+**💵 Below your target by:** ${(desired_price - product.sale_price):.2f} ({target_discount:.1f}%)
 
-    delta_s = (current_time - time_obj).total_seconds()
-
-    return get_days_hours(delta_s)
+🔗 [{product.product_url}]({product.product_url})"""
 
 
-def sale_ntfy(name, sale_price, regular_price, dollar_savings, percent_savings, url, price_update_date):
-    days, hours = calc_time_diff(price_update_date)
-    relative_time_str = f"{int(days)} days, {hours} hours ago"
+def in_stock(product):
+    return f"""### {product.product_name} is back in stock!
 
-    return f"""### {name} is on sale!
+**💰 Current Price:** ${product.regular_price:.2f}
 
-**New Price:** ${sale_price}
-
-**Original Price:** ${regular_price}
-
-**Discount Amount:** ${dollar_savings}
-
-**Discount Percent:** {percent_savings}%
-
-[{url}]({url})
-
----
-
-Price was last updated on {price_update_date} ({relative_time_str})"""
-
-
-def non_sale_ntfy(name, regular_price, desired_price, url, price_update_date):
-    days, hours = calc_time_diff(price_update_date)
-    relative_time_str = f"{int(days)} days, {hours} hours ago"
-
-    if regular_price < desired_price:
-        dollar_savings = desired_price - regular_price
-        dollar_savings = round(dollar_savings, 2)
-        percent_savings = (100 - (regular_price / desired_price)) * 100
-        percent_savings = round(percent_savings, 1)
-
-        return f"""### {name} is now in stock!
-
-**Price:** ${regular_price}
-
-**Your Desired Price:** ${desired_price}
-
-**Dollar Savings:** ${dollar_savings}
-
-**Percent Savings:** {percent_savings}%
-
-[{url}]({url})
-
----
-
-Price was last updated on {price_update_date} ({relative_time_str})"""
-
-    else:
-        return f"""### {name} is now in stock!
-
-**Price:** ${regular_price}
-
-**Your Desired Price:** ${desired_price}
-
-[{url}]({url})
-
----
-
-Price was last updated on {price_update_date} ({relative_time_str})"""
+🔗 [{product.product_url}]({product.product_url})"""
 
 
 if __name__ == "__main__":
-    # test send of sale ntfy
-    res = sale_ntfy(
+    from .schema import Product
+    # test all ntfy templates
+    p = Product(
+        "B0FQFB8FMG",
         "airpods pro 3",
+        True,
+        True,
         199.99,
         249.99,
-        50.0,
-        "20.0",
-        "https://api.bestbuy.com/click/-/6376563/pdp",
-        "2026-01-23T00:00:54"
+        "https://amazon.com/dp/airpods-pro-3",
+        "amazon",
+        "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
     )
 
-    with open("temp_sale.md", "w", encoding="utf-8") as f:
-        f.write(res)
-
-    # test send of non sale ntfy
-    res = non_sale_ntfy(
-        "LG UltraGear 27",
-        349.99,
-        400,
-        "https://api.bestbuy.com/click/-/6575404/pdp",
-        "2025-12-18T02:01:15"
-    )
-
-    with open("temp_non_sale.md", "w", encoding="utf-8") as f:
-        f.write(res)
-
-
-# TODO: add type hinting to function defs
+    print(on_sale(p, 300.00))
+    print()
+    print(below_max_price(p, 300))
+    print()
+    print(in_stock(p))
